6e5c1e171536bf8ec3160377d1ecfe862f6080a3
AgentTango <aditya1999thakur@gmail.com>
2018-09-03 01:49:04 +0530

    Initial Commit

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour {
+
+	/// <summary>
+	/// OnCollisionEnter is called when this collider/rigidbody has begun
+	/// touching another rigidbody/collider.
+	/// </summary>
+	/// <param name="other">The Collision data associated with this collision.</param>
+	public PlayerMovement movement;		
+	void OnCollisionEnter(Collision other)
+	{
+		Debug.Log(other.collider.name);
+		if(other.collider.tag=="Obstacle")
+			movement.enabled = false;
+	}
+}
